66d528ef38a3bc4bd13209281b3c2859972c06fb
AbdukodirSoliev <Abdukodir_Soliev@epam.com>
2022-02-24 01:26:52 +0500

    feat: try to pass test

--- solution.go
+++ solution.go
@@ -4,17 +4,17 @@ import "math"
 
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
-// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
+// Define constants to represent 0, 3 and 4 sides. Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
 // it's like:
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum int) float64 {
-	const SidesSquare = 4
-	const SidesTriangle = 3
-	const SidesCircle = 0
+const SidesSquare = 4
+const SidesTriangle = 3
+const SidesCircle = 0
 
+func CalcSquare(sideLen float64, sidesNum int) float64 {
 	var area float64
 	area = 0
 
