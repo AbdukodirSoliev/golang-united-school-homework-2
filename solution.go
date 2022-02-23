e7c5f4db71f4c0f52a2eb146a882d28ddcf63a51
Abdukodir <asoliev@ebsco.com>
2022-02-24 01:13:48 +0500

    calculated area of figures

--- solution.go
+++ solution.go
@@ -1,5 +1,7 @@
 package square
 
+import "math"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +10,20 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum int) float64 {
+	const SidesSquare = 4
+	const SidesTriangle = 3
+	const SidesCircle = 0
+
+	var area float64
+	area = 0
+
+	if sidesNum == SidesSquare {
+		area = sideLen * sideLen
+	} else if sidesNum == SidesTriangle {
+		area = (math.Sqrt(3) / 4) * sideLen * sideLen
+	} else if sidesNum == SidesCircle {
+		area = math.Pi * sideLen * sideLen
+	}
+	return area
 }
